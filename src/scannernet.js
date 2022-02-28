a0d903c2f653e9b0e6107af1b3b3a70a2033ca99
Chrisdigity <chrisdigity@gmail.com>
2022-02-28 11:46:34 +1000

    reduce socket spam from net scanner

--- src/scannernet.js
+++ src/scannernet.js
@@ -4,6 +4,96 @@
  * For more information, see License.md
  */
 
+/**
+ * Determines if the provided variable is empty/null.
+ **
+ * Mainly for arrays and objects but can be used on simple variables.
+ * Main Test cases:
+ *    {}            -> true
+ *    []            -> true
+ *    [null]        -> true
+ *    {a:null}      -> false
+ *    {a:null, b:1} -> false
+ *    {a:1, b:null} -> false
+ *    [null, 1]     -> false
+ *    [1, null]     -> false
+ */// Returns, Boolean
+function isEmpty (variable) {
+  // Handle Objects and Arrays
+  if (typeof variable === 'object') {
+    for (const key in variable) {
+      // Arrays containing any non-NULL value are NOT considered empty
+      if ((variable.constructor === Array && variable[key]) ||
+      // Objects containing null values for valid keys are NOT considered empty
+        (variable.constructor !== Array && variable)) {
+        return false;
+      }
+    }
+  } else
+  // Handle all other
+  if (variable) return false;
+
+  return true;
+}
+
+/**
+ * Simple Object duplication.
+ **
+ * Mainly used for breaking free of referenced objects.
+ */// Returns, Object (duplicate)
+function dupObj (obj) {
+  return JSON.parse(JSON.stringify(obj));
+}
+
+/* diffObj(), A RECURSIVE function to detect the differences
+ * that exist between 2 different states of the same object.
+ **
+ * Depends on: isEmpty()
+ * Used to reduce bandwidth by upwards of 95%.
+ * (compared to the first revision of bandwidth transmission)
+ **
+ */// Returns difference, else false on no difference
+function diffObj (curr, old) {
+  const changes = {};
+  for (const key in curr) {
+    if (!(key in old)) changes[key] = curr[key];
+  }
+  for (const key in old) {
+    if (!(key in curr)) { changes[key] = undefined; continue; }
+    if (old[key] && typeof old[key] === 'object' &&
+      ((typeof old[key] === typeof curr[key]))) {
+      // Handle Arrays
+      // * add -> typeof curr[key][0] !== 'object'
+      // * to conditions if objects are expected in arrays
+      if (old[key].constructor === Array &&
+        ((old[key].constructor === curr[key].constructor))) {
+        const tempA = [];
+        // scan 'old' for absent items, mark to delete
+        for (let i = 0; i < old[key].length; i++) {
+          if (curr[key].indexOf(old[key][i]) < 0) {
+            tempA[i] = undefined;
+          }
+        }
+        // scan 'curr' for new items, fill deleted or append
+        for (let i = 0; i < curr[key].length; i++) {
+          if (old[key].indexOf(curr[key][i]) < 0) {
+            const fill = tempA.indexOf(undefined);
+            if (fill > -1) tempA[fill] = curr[key][i];
+            else tempA[old[key].length + i] = curr[key][i];
+          }
+        }
+        // copy changes to 'curr' else no changes
+        if (tempA.length) changes[key] = tempA;
+      } else {
+        // Handle Objects
+        const recursivechanges = diffObj(curr[key], old[key]);
+        if (recursivechanges) changes[key] = recursivechanges;
+      }
+    } else if (old[key] !== curr[key]) changes[key] = curr[key];
+  }
+  return isEmpty(changes) ? false : changes;
+}
+
 /* constants */
 const MS7DAY = 7 * 24 * 60 * 60 * 1000;
 const MS3DAY = 3 * 24 * 60 * 60 * 1000;
@@ -138,6 +228,8 @@ class NetworkScanner {
   }
 
   scan (ip) {
+    // avoid non-localhost private ips
+    if (NetworkScanner.isPrivateIPv4(ip)) return;
     // avoid duplicate scans
     if (this.scanning.has(ip)) return;
     // add 1 second delay scans exceeding maximum
@@ -173,20 +265,23 @@ class NetworkScanner {
           }
         }
       } else node.uptimestamp = 0;
+      // calculate differences between cached node
+      const changes = diffObj(node, cached);
+      // ensure ip is always delivered with changes
+      if (changes) changes.ip = node.ip;
       // update cached node object
       Object.assign(cached, node);
       // if VEOK and not in cache...
       if (cached.status === Mochimo.VEOK && !this.cache.has(ip)) {
-        // ... add node to cache, and
-        if (!NetworkScanner.isPrivateIPv4(ip)) this.cache.set(ip, cached);
-        // ... extend scan to peers of valid node
+        // ... update cache, and extend scan to peers of valid node
+        this.cache.set(ip, cached);
         if (Array.isArray(cached.peers)) {
           cached.peers.forEach(this.scan.bind(this));
         }
       }
-      // emit update to streams
-      if (typeof this.emit === 'function') {
-        this.emit(cached, 'network');
+      // emit changes to stream where ip is in cache
+      if (changes && this.cache.has(ip) && typeof this.emit === 'function') {
+        this.emit(changes, 'network');
       }
     }
   }
