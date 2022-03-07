2e9cff787fd99f527eb764e386e31c59fa2235ed
Chrisdigity <chrisdigity@gmail.com>
2022-03-07 11:33:02 +1000

    enable complex union queries

--- src/dbmysql.js
+++ src/dbmysql.js
@@ -4,10 +4,10 @@
  * For more information, see License.md
  */
 
-const interpreter = /^([0-9a-z]+)(?:(<>|<=|>=|<|>|=)([0-9a-z-.*]+))?$/i;
-const separators = /[?&|]/g;
-
 const mysql2 = require('mysql2');
+
+const paramRegex = /^([0-9a-z]+)(?:(<>|<|<=|=|>=|>)([0-9a-z-.*]+))?$/i;
+const paramSeparatorRegex = /[?&|]/g;
 
 const stdOpts = {
   waitForConnections: true,
@@ -18,22 +18,18 @@ const stdOpts = {
   timezone: 'Z'
 };
 
-function requestHandler (table, options, callback) {
-  let { limit, offset, orderby, search, select } = options;
-  let groupCondition = null;
-  let nConditions = 0;
-  let where = '';
+function queryComponents ({ limit = 10, offset = 0, search, where = '' }) {
   // consume search and place in `where`
   while (search) {
-    const nextbrkp = search.slice(1).search(separators) + 1;
+    const nextbrkp = search.slice(1).search(paramSeparatorRegex) + 1;
     const condition = nextbrkp
       ? search.slice(1, nextbrkp)
       : search.slice(1);
     // check condition
     if (condition) {
-      const matched = condition.match(interpreter);
+      const matched = condition.match(paramRegex);
       if (matched) {
-        // drop match
+        // drop initial match
         matched.shift();
         let [column, comparitor, value] = matched;
         // check for "special" parameters
@@ -43,7 +39,7 @@ function requestHandler (table, options, callback) {
               // cleanse limit value
               limit = isNaN(value) ? 10 : Number(value);
               if (limit > 100) limit = 100;
-              if (limit < 0) limit = 0;
+              if (limit < 1) limit = 1;
               break;
             case 'offset':
               // cleanse offset value
@@ -51,23 +47,24 @@ function requestHandler (table, options, callback) {
               if (offset < 0) offset = 0;
               break;
             default:
-              if (value.includes('*')) {
-                value = value.replace(/[*]/g, '%');
-                if (comparitor === '<>') comparitor = 'NOT LIKE';
-                else if (comparitor === '=') comparitor = 'LIKE';
-              }
-              // apply condition extension and modified condition
-              if (nConditions++) {
-                // encase existing conditions on groupCondition change
-                if (groupCondition && groupCondition !== search.charAt(0)) {
-                  where = `( ${where} )`;
-                }
-                groupCondition = search.charAt(0);
-                // apply condition extension
-                if (groupCondition === '&') where += ' AND ';
-                if (groupCondition === '|') where += ' OR ';
-              }
-              where += `\`${column}\` ${comparitor} '${value}'`;
+              value = value.toLowerCase();
+              if (['<>', '='].includes(comparitor)) {
+                if (value === 'null') {
+                  // special treatment for null values
+                  value = 'NULL';
+                  if (comparitor === '<>') comparitor = 'IS NOT';
+                  else if (comparitor === '=') comparitor = 'IS';
+                } else if (value.includes('*')) {
+                  // special treatment for wildcard values
+                  value = `'${value.replace(/[*]/g, '%')}'`;
+                  if (comparitor === '<>') comparitor = 'NOT LIKE';
+                  else if (comparitor === '=') comparitor = 'LIKE';
+                } else value = `'${value}'`;
+              } else value = `'${value}'`;
+              // apply condition extension
+              if (where) where += ' AND ';
+              // apply where condition
+              where += `${column} ${comparitor} ${value}`;
           }
         }
       }
@@ -75,15 +72,36 @@ function requestHandler (table, options, callback) {
     // reduce search for next round
     search = nextbrkp ? search.slice(nextbrkp) : '';
   }
-  // perform restricted query
-  this.query(`
-    SELECT ${
-      Array.isArray(select) ? select.join(', ') : (select || '*')}
-    from \`${table}\`
-    ${where ? `WHERE ${where}` : ''}
-    ${orderby ? `ORDER BY ${orderby}` : ''}
-    ${limit ? `LIMIT ${limit}` : 'LIMIT 10'}
-    ${offset ? `OFFSET ${offset}` : ''}
+  // return components as object
+  return { limit, offset, where };
+}
+
+function requestHandler (table, options, callback) {
+  // extract query components from search and options
+  const { limit, offset, orderby, select = '*', union, where } = {
+    ...options, ...queryComponents({ search: options.search })
+  };
+  if (Array.isArray(union) && union.length) {
+    const queries = union.map((subsearch) => {
+      const sub = queryComponents({
+        limit: limit + offset, search: subsearch, where
+      });
+      return `SELECT ${select} FROM \`${table}\`
+        ${sub.where ? `WHERE ${sub.where}` : ''}
+        ${orderby ? `ORDER BY ${orderby}` : ''}
+        LIMIT ${sub.offset}, ${sub.limit}`;
+    });
+    // execute compound (union) query
+    return this.query(`
+      (${queries.join(') UNION DISTINCT (')})
+      ${orderby ? `ORDER BY ${orderby}` : ''}
+      LIMIT ${offset}, ${limit}
+    `, callback);
+  }
+  // execute simple query
+  return this.query(`
+    SELECT ${select || '*'} FROM \`${table}\` ${where ? `WHERE ${where}` : ''}
+    ${orderby ? `ORDER BY ${orderby}` : ''} LIMIT ${offset}, ${limit}
   `, callback);
 }
 
